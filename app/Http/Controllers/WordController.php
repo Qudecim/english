f89966d879deb2b32918b8970caa4790ccc86f7f
Qudecim <99prblmsaint1@gmail.com>
2023-03-11 22:31:13 +0300

    Made simply WordsController

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\WordCollection;
+use App\Http\Resources\WordResource;
+use App\Models\Word;
 use Illuminate\Http\Request;
 
 class WordController extends Controller
@@ -18,32 +20,52 @@ class WordController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): WordResource
     {
-        //
+        $request->validate([
+            'word_1' => 'required|max:255',
+            'word_2' => 'required|max:255',
+        ]);
+
+        $word = new Word();
+        $word->user_id = $request->user()->id;
+        $word->word_1 = $request->word_1;
+        $word->word_2 = $request->word_2;
+        $word->save();
+
+        return new WordResource($word);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Word $word): WordResource
     {
-        //
+        return new WordResource($word);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Word $word): WordResource
     {
-        //
+        $request->validate([
+            'word_1' => 'required|max:255',
+            'word_2' => 'required|max:255',
+        ]);
+
+        $word->word_1 = $request->word_1;
+        $word->word_2 = $request->word_2;
+        $word->save();
+
+        return new WordResource($word);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Word $word): WordResource
     {
-        //
+        return new WordResource($word);
     }
 }
